9205e269dfdd9fed755cb939d801d67a59ad711f
Lili Huynh <lillianhuynh312@gmail.com>
2021-02-26 15:23:35 -0800

    create schema and seeds for burgers data

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Fish');
+INSERT INTO burgers (name) VALUES ('Combo');
+INSERT INTO burgers (name, eaten) VALUES ('Vegetable', true);
+INSERT INTO burgers (name, eaten) VALUES ('Beef', true);
+INSERT INTO burgers (name, eaten) VALUES ('Pork', true);
+INSERT INTO burgers (name) VALUES ('Hawaii');
