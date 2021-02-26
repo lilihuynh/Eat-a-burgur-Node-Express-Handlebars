1fd0498aecdaa6297630a020321d73cdf9e486be
Lili Huynh <lillianhuynh312@gmail.com>
2021-02-26 15:30:43 -0800

    Helper function for SQL syntax

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// Helper function for SQL syntax.
+// helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
